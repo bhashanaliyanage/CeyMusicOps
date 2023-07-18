0ec23fde79aa40391775d3591ee4ab07b0ac46c2
Bhashana Liyanage <it19972794@my.sliit.lk>
2023-07-19 02:30:12 +0530

    Database Connection Established

--- src/App.java
+++ src/App.java
@@ -13,11 +13,23 @@ public class App {
                 String[] row = line.split(",");
                 if (row.length >= 2) { // Ensure at least two values exist in the row
                     String isrc = row[0];
-                    System.out.println("Here!");
                     String earnings = row[1]; // Remove double quotes and convert earnings to double
 
-                    System.out.println("ISRC: " + isrc);
-                    System.out.println("Earnings: " + earnings);
+                    String ISRCWithoutQuotes = isrc.replace("\"", "");
+                    String earningsWithoutQuotes = earnings.replace("\"", "");
+
+                    System.out.println("ISRC: " + ISRCWithoutQuotes);
+                    System.out.println("Earnings in EUR: " + earningsWithoutQuotes);
+
+                    if (!ISRCWithoutQuotes.equals("") && !earningsWithoutQuotes.equals("Reported Royalty")) {
+                        Boolean done = Database.addEntry(ISRCWithoutQuotes, earningsWithoutQuotes);
+                        if (done == true) {
+                            System.out.println("Earning Added...");
+                        } else {
+                            System.out.println("Database Entry Faliure...");
+                        }
+                        Database.closeConnection();
+                    }
                     System.out.println();
                 }
             }
---
+++ src/Database.java
@@ -0,0 +1,60 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.text.BreakIterator;
+
+public class Database {
+    // Connection parameters
+    private static String url = "jdbc:mysql://localhost:3306/ceymusicops";
+    private static String username = "root";
+    private static String password = "admin";
+    private static boolean querySuccess;
+
+    public static Connection con;
+
+    public static Connection utilizeConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection(url, username, password);
+            System.out.println("Connected to CeyMusicOps via Java SQL...");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return con;
+    }
+
+    public static boolean addEntry(String ISRC, String amount) {
+        PreparedStatement pst;
+        int rs;
+        double amountConverted = Double.parseDouble(amount);
+
+        try {
+            con = Database.utilizeConnection();
+            pst = con.prepareStatement("INSERT INTO earnings (ISRC, EUR) VALUES (?, ?)");
+            pst.setString(1, ISRC);
+            pst.setDouble(2, amountConverted);
+            rs = pst.executeUpdate();
+            if (rs > 0) {
+                querySuccess = true;
+            } else {
+                querySuccess = false;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return querySuccess;
+    }
+    
+    public static void closeConnection() {
+        try {
+            if (con != null) {
+                con.close();
+                System.out.println("Connection closed.");
+            }
+        } catch (SQLException e) {
+            System.out.println("Error closing the connection.");
+            e.printStackTrace();
+        }
+    }
+}
